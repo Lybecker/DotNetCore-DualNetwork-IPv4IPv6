84a0825a411c5964e094f8f4922055bbb1414252
Anders Lybecker <anders@lybecker.com>
2018-08-22 21:04:40 +0200

    UDP sample showing how to use the same socket for both IPv4 and IPv6. Works both in server and client side.

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int port = 8080;
+            //var address = await Dns.GetHostAddressesAsync("localhost");
+
+            string message = "This is a test message";
+            var data = Encoding.ASCII.GetBytes(message);
+
+            using (var socket = new UdpClient(AddressFamily.InterNetworkV6))
+            {
+                // Enable Dual Mode, which means the Socket can listen on both IPv4 and IPv6 at the same time.
+                socket.Client.DualMode = true;
+
+                socket.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                
+                var ipv4Address = "127.0.0.1"; // IPAddress.Loopback
+                socket.Connect(IPAddress.Parse(ipv4Address), port);
+                await socket.SendAsync(data, data.Length);
+                
+                var ipv6Address = "::1"; // IPAddress.IPv6Loopback
+                socket.Connect(IPAddress.Parse(ipv6Address), port);
+                await socket.SendAsync(data, data.Length);
+            }
+            Console.WriteLine("Done sending");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endpoint = new IPEndPoint(IPAddress.IPv6Any, 8080);
+
+            // I'm using the UdpClient as a server in this case like https://docs.microsoft.com/en-us/dotnet/framework/network-programming/using-udp-services
+            var listener = new UdpClient(AddressFamily.InterNetworkV6);
+
+            // Enable Dual Mode, which means the Socket can listen on both IPv4 and IPv6 at the same time.
+            listener.Client.DualMode = true;
+            // Setting the DualMode to true, is the same as doing the below:
+            //listener.Client.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+
+            // Start listening on the endpoint
+            listener.Client.Bind(endpoint);
+            
+            // The listender Client can only show one endpoint, but Socket is listening on both IPv4 and IPv6
+            Console.WriteLine($"Listining for data on {listener.Client.LocalEndPoint} ...");
+            while (true)
+            {
+                var result = await listener.ReceiveAsync();
+                
+                string data = Encoding.ASCII.GetString(result.Buffer);
+
+                var address = result.RemoteEndPoint.Address.IsIPv4MappedToIPv6 ? result.RemoteEndPoint.Address.MapToIPv4() : result.RemoteEndPoint.Address;
+
+                Console.WriteLine($"Received data '{data}' from {address}");
+            }
+        }
+    }
+}
