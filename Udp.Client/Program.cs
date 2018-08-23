5b6ae3a4d54400dd763f51f663eb31c5416566ad
Anders Lybecker <anders@lybecker.com>
2018-08-23 09:44:32 +0200

    Renamed projects prepended Udp

--- Udp.Client/Program.cs
+++ Udp.Client/Program.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client
+namespace Udp.Client
 {
     class Program
     {
--- Udp.Server/Program.cs
+++ Udp.Server/Program.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+namespace Udp.Server
 {
     class Program
     {
