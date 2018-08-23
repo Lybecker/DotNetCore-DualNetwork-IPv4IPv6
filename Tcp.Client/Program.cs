64397fe672a48213de6f17a7db7523d7147cbc77
Anders Lybecker <anders@lybecker.com>
2018-08-23 11:18:57 +0200

    TCPClient sample with dual mode IPv4 and IPv6 added

---
+++ Tcp.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp.Client
+{
+    class Program
+    {
+        const int PORT = 8080;
+
+        static async Task Main(string[] args)
+        {
+            TcpClient socket = new TcpClient(AddressFamily.InterNetworkV6);
+            socket.Client.DualMode = true;
+            var ipv4Address = IPAddress.Parse("127.0.0.1"); // IPAddress.Loopback
+            await ConnectSendReceive(socket, ipv4Address);
+
+            socket = new TcpClient(AddressFamily.InterNetworkV6);
+            socket.Client.DualMode = true;
+            var ipv6Address = IPAddress.Parse("::1"); // IPAddress.IPv6Loopback
+            await ConnectSendReceive(socket, ipv6Address);
+
+        }
+
+        static async Task ConnectSendReceive(TcpClient socket, IPAddress ipAddress)
+        {
+            Console.WriteLine($"Connecting to {ipAddress}...");
+            await socket.ConnectAsync(ipAddress, PORT);
+
+            string message = "This is a test message";
+            var data = Encoding.ASCII.GetBytes(message);
+
+            using (NetworkStream ns = socket.GetStream())
+            {
+                Console.WriteLine("Writing to server");
+                await ns.WriteAsync(data, 0, data.Length);
+
+                byte[] bytes = new byte[1024];
+                int bytesRead = await ns.ReadAsync(bytes, 0, bytes.Length);
+                Console.WriteLine($"Response '{Encoding.ASCII.GetString(bytes, 0, bytesRead)}");
+            }
+
+            socket.Close();
+        }
+    }
+}
---
+++ Tcp.Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endpoint = new IPEndPoint(IPAddress.IPv6Any, 8080);
+
+            var listener = new TcpListener(endpoint);
+            // Enable Dual Mode, which means the Socket can listen on both IPv4 and IPv6 at the same time.
+            listener.Server.DualMode = true;
+            // Setting the DualMode to true, is the same as doing the below:
+            //listener.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+            listener.Start();
+
+            while (true)
+            {
+                // The listender Client can only show one endpoint, but Socket is listening on both IPv4 and IPv6
+                Console.Write($"Waiting for connections {listener.LocalEndpoint} ...");
+                TcpClient client = await listener.AcceptTcpClientAsync();
+
+                Console.WriteLine($"Connection accepted.");
+
+                using (NetworkStream ns = client.GetStream())
+                {
+                    byte[] bytes = new byte[1024];
+                    int bytesRead = await ns.ReadAsync(bytes, 0, bytes.Length);
+                    var message = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+
+                    var remoteEndpoint = ((IPEndPoint)client.Client.RemoteEndPoint);
+                    var address = remoteEndpoint.Address.IsIPv4MappedToIPv6 ? remoteEndpoint.Address.MapToIPv4() : remoteEndpoint.Address;
+                    Console.WriteLine($"Received data '{message}' from {address}");
+
+                    byte[] byteTime = Encoding.ASCII.GetBytes($"Thanks for the message at {DateTime.Now.ToString()}");
+                    ns.Write(byteTime, 0, byteTime.Length);
+                }
+                client.Close();
+            }
+
+            //listener.Stop();
+        }
+    }
+}
